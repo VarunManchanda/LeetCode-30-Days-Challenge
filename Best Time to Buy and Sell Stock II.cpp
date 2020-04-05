a8de63061bbb809e8a72d139c4c18797593a9870
Varun Manchanda <33371013+VarunManchanda@users.noreply.github.com>
2020-04-05 14:49:02 +0530

    Create Best Time to Buy and Sell Stock II.cpp

---
+++ Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,22 @@
+/*Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+
+*/
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size()==0) return 0;
+        int ans = 0;
+        for(int i=1; i<prices.size(); i++)
+        {
+            if(prices[i]-prices[i-1]>=0)
+            {
+                ans += prices[i]-prices[i-1];
+            }
+        }
+        return ans;
+    }
+};
