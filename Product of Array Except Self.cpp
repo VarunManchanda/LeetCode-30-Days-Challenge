9dd60135395238de294e5f475888a54b2e5607bb
Varun Manchanda <33371013+VarunManchanda@users.noreply.github.com>
2020-04-15 15:39:06 +0530

    Update Product of Array Except Self.cpp

--- Product of Array Except Self.cpp
+++ Product of Array Except Self.cpp
@@ -9,12 +9,14 @@ public:
         _fast_;
         int n = nums.size();
         vector<int> pref(n), suff(n), ans(n);
-        pref[0] = 1;
-        suff[n-1] = 1;
-        for(int i=1; i<n; i++) pref[i] = pref[i-1] * nums[i-1];
-        for(int i=0; i<n-1; i++) suff[n-i-2] = suff[n-i-1] * nums[n-i-1];
-        for(int i=0; i<n; i++) ans[i] = pref[i]*suff[i];
+        pref[0]=nums[0]; suff[n-1]=nums[n-1];
+        for(int i=1; i<n; i++)    pref[i] = pref[i-1] * nums[i];
+        for(int i=n-2; i>=0; i--) suff[i] = suff[i+1] * nums[i];
+        for(int i=1; i<n-1; i++)   ans[i] = pref[i-1] * suff[i+1];
+        ans[0] = 1 * suff[1];
+        ans[n-1] = pref[n-2] * 1;
         return ans;
     }
+    
     
 };
